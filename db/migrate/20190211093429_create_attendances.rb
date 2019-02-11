eec901f0fe79493f5d0992a30318afe2e88ffa5b
Jordan Aumis <jordan0aumis@gmail.com>
2019-02-11 11:03:19 +0100

    new psuh

--- db/migrate/20190211093429_create_attendances.rb
+++ db/migrate/20190211093429_create_attendances.rb
@@ -1,7 +1,9 @@
 class CreateAttendances < ActiveRecord::Migration[5.2]
   def change
     create_table :attendances do |t|
-
+      t.string :stripe_curtomer_id
+      t.belongs_to :user
+      t.belongs_to :event
       t.timestamps
     end
   end
